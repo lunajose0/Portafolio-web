7852e16c00da6fae14413b788970e773bf5fb0af
Jose Luna <141267858+lunajose0@users.noreply.github.com>
2025-01-27 17:38:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+
+    function showSlide(index) {
+        const slides = document.querySelectorAll('.slide');
+        if (index >= slides.length) {
+            currentSlide = 0;
+        } else if (index < 0) {
+            currentSlide = slides.length - 1;
+        } else {
+            currentSlide = index;
+        }
+        const offset = -currentSlide * 100; // Calcula el desplazamiento
+        document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+    }
+
+    function changeSlide(direction) {
+        showSlide(currentSlide + direction);
+    }
+
+    // Muestra la primera diapositiva al cargar
+    showSlide(currentSlide);
